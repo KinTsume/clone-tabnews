8de50a149045674115c099e7801b47ae94221a31
KinTsume <calebbneto@gmail.com>
2025-10-30 19:02:32 -0300

    Add the chest opening functionality

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,35 @@
+import Image from "next/image";
+import { useState, useRef } from "react"
+
 function Home() {
-    return <h1>Olá! Clique <a href="https://share.google/xYxJOviZLYZvECkuu">neste link</a> ganhar 1 milhão de reais!!!</h1>
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openChest = () => {
+        setIsOpen(true)
+    }
+
+    return (
+        <div style={{display: "flex", flexDirection: 'column', alignItems: "center"}}>
+            <h1>You found a chest on the dungeon! Click it to claim your treasure.</h1>
+            {isOpen ? 
+                <div>
+                    <h1>Oh no! It's a rick rolling mimic!</h1>
+                    <Image src={'/mimic.png'} width={500} height={500}>
+                        
+                    </Image>     
+                    <video 
+                    style={{position: 'absolute', left: '45%', top: '45%'}}
+                    width="144" 
+                    height="144" 
+                    controls 
+                    autoPlay>
+                        <source src="/never-gonna-give-you-up.mp4" type="video/mp4"/>
+                    </video>               
+                </div> : 
+                <Image onClick={() => openChest()} src={'/closed-chest.png'} width={500} height={500}></Image>
+            }
+            
+        </div>
+    )
 }
 export default Home;
