98776b4532bcfea3dde674c5aa8c077a44a8bfc4
KinTsume <calebbneto@gmail.com>
2025-10-30 19:24:22 -0300

    Adjust the app layout to mobile

--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,24 @@ function Home() {
     }
 
     return (
-        <div style={{display: "flex", flexDirection: 'column', alignItems: "center"}}>
+        <div style={{display: "flex", flexDirection: 'column', alignItems: "center", textAlign: 'center'}}>
             <h1>You found a chest on the dungeon! Click it to claim your treasure.</h1>
             {isOpen ? 
                 <div>
                     <h1>Oh no! It's a rick rolling mimic!</h1>
-                    <Image src={'/mimic.png'} width={500} height={500}>
+                    <Image src={'/mimic.png'} width={250} height={250}>
                         
                     </Image>     
                     <video 
-                    style={{position: 'absolute', left: '45%', top: '45%'}}
-                    width="144" 
-                    height="144" 
+                    style={{position: 'absolute', left: '50%', top: '65%', transform: 'translate(-50px, -50px)', marginRight: 'auto'}}
+                    width="100" 
+                    height="100" 
                     controls 
                     autoPlay>
                         <source src="/never-gonna-give-you-up.mp4" type="video/mp4"/>
                     </video>               
                 </div> : 
-                <Image onClick={() => openChest()} src={'/closed-chest.png'} width={500} height={500}></Image>
+                <Image onClick={() => openChest()} src={'/closed-chest.png'} width={250} height={250}></Image>
             }
             
         </div>
