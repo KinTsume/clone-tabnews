26f0b10c005c87c491efa49f6656ab678e1ffc51
KinTsume <calebbneto@gmail.com>
2026-01-14 20:17:42 -0300

    Finaliza o endpoint `/api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,19 @@
-test("GET to /api/v1/status shouls return 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  const databaseBody = responseBody.dependencies.database;
+
+  expect(databaseBody.version).toBe("16.11");
+
+  expect(databaseBody.max_connections).toBe(100);
+
+  expect(databaseBody.opened_connections).toBe(1);
 });
