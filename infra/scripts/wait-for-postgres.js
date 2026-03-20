bf374ebd38d6eb49093e348bd032791790bab5fd
KinTsume <calebbneto@gmail.com>
2026-03-20 09:12:14 -0300

    Adiciona script `wait-for-postgres`

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,21 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec(
+    "sudo docker exec postgres-dev pg_isready --host localhost",
+    handleReturn,
+  );
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n🟢 Postgres esta pronto e aceitando conexões!\n");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
+checkPostgres();
