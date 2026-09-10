78d9935f3043c2f828c5a54491fe405264bf9c6f
KinTsume <calebbneto@gmail.com>
2026-09-10 18:21:20 -0300

    feat: add `findOneByEmail()` on `user.js` model

--- models/user.js
+++ models/user.js
@@ -32,6 +32,35 @@ async function findOneByUsername(username) {
   }
 }
 
+async function findOneByEmail(email) {
+  const userFound = await runSelectQuery(email);
+
+  return userFound;
+
+  async function runSelectQuery(email) {
+    const result = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)
+      LIMIT
+        1`,
+      values: [email],
+    });
+
+    if (result.rowCount <= 0) {
+      throw new NotFoundError({
+        message: "O email informado não foi encontrado no sistema.",
+        action: "Verifique se o email digitado está correto.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
 async function create(userInputValues) {
   await validateUniqueUsername(userInputValues.username);
   await validateUniqueEmail(userInputValues.email);
@@ -160,6 +189,7 @@ async function hashPasswordInObject(userInputValues) {
 const user = {
   create,
   findOneByUsername,
+  findOneByEmail,
   update,
 };
 
